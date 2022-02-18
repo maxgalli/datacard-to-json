9a735c62476dc03a8fbe97b20f99c3c3ab374aea
Massimiliano Galli <massimiliano.galli@cern.ch>
2022-02-18 10:13:51 +0100

    go back to have path to files instead of proper histogram, in line with combine's way of doing stuff

--- datacard_to_json.py
+++ datacard_to_json.py
@@ -1,7 +1,6 @@
 import json
 import logging
 import sys
-import uproot
 from copy import deepcopy
 from typing import Any
 
@@ -13,23 +12,30 @@ def json_str(obj: Any) -> str:
     return json.dumps(obj, sort_keys=True, indent=4)
 
 
-def restructure_shapes(shape_lines: list, channels: list, samples: list, systematics: list, mass: str) -> list:
-    samples.append("data_obs") # NBBBB!!!!!!!
+def restructure_shapes(
+    shape_lines: list, channels: list, samples: list, systematics: list, mass: str
+) -> list:
+    samples.append("data_obs")  # NBBBB!!!!!!!
     shape_lines_reco = []
     shape_dict_list = []
     identifiers = [
-        "type", "sample", "channel", "file", "histogram", "histogram_with_systematics"
+        "type",
+        "sample",
+        "channel",
+        "file",
+        "histogram",
+        "histogram_with_systematics",
     ]
     for shape_line in shape_lines:
         line_dict = {}
         for id, word in zip(identifiers, shape_line.split()):
             line_dict[id] = word
-        
+
         shape_lines_reco.append(line_dict)
 
-    #log.debug("shape_lines_reco:\n{}".format(shape_lines_reco))
+    # log.debug("shape_lines_reco:\n{}".format(shape_lines_reco))
     for shape_line in shape_lines_reco:
-        #log.debug(shape_line)
+        # log.debug(shape_line)
         # main
         if shape_line["sample"] == "*":
             # remove samples that are already in other lines
@@ -52,7 +58,7 @@ def restructure_shapes(shape_lines: list, channels: list, samples: list, systema
         else:
             l_channels = [shape_line["channel"]]
         l_channels = list(set(l_channels))
-        #log.debug("l_channels = {}".format(l_channels))
+        # log.debug("l_channels = {}".format(l_channels))
 
         for channel in l_channels:
             for sample in l_samples:
@@ -61,34 +67,45 @@ def restructure_shapes(shape_lines: list, channels: list, samples: list, systema
                         "sample": sample,
                         "channel": channel,
                         "file": shape_line["file"],
-                        "histogram": shape_line["histogram"].replace("$CHANNEL", channel).replace("$MASS", mass).replace("$PROCESS", sample)
+                        "histogram": shape_line["histogram"]
+                        .replace("$CHANNEL", channel)
+                        .replace("$MASS", mass)
+                        .replace("$PROCESS", sample),
                     }
                 )
 
         # systematics
         if "histogram_with_systematics" in shape_line:
-            #log.debug("Now creating systematics for shape_line \n{}".format(shape_line))
+            # log.debug("Now creating systematics for shape_line \n{}".format(shape_line))
             for sys in systematics:
                 for channel in l_channels:
                     for sample in l_samples:
-                        #log.debug("shape_line:\n{}\nsys: {}, channel: {}, sample: {}".format(shape_line, sys, channel, sample))
+                        # log.debug("shape_line:\n{}\nsys: {}, channel: {}, sample: {}".format(shape_line, sys, channel, sample))
                         shape_dict_list.append(
                             {
                                 "sample": sample,
                                 "channel": channel,
                                 "file": shape_line["file"],
                                 "modifier": sys,
-                                "histogram_up": "{}Up".format( 
-                                shape_line["histogram_with_systematics"].replace("$CHANNEL", channel).replace("$MASS", mass).replace("$PROCESS", sample).replace("$SYSTEMATIC", sys)
-                                    ),
-                                "histogram_down": "{}Down".format( 
-                                shape_line["histogram_with_systematics"].replace("$CHANNEL", channel).replace("$MASS", mass).replace("$PROCESS", sample).replace("$SYSTEMATIC", sys)
-                                    ),
+                                "histogram_up": "{}Up".format(
+                                    shape_line["histogram_with_systematics"]
+                                    .replace("$CHANNEL", channel)
+                                    .replace("$MASS", mass)
+                                    .replace("$PROCESS", sample)
+                                    .replace("$SYSTEMATIC", sys)
+                                ),
+                                "histogram_down": "{}Down".format(
+                                    shape_line["histogram_with_systematics"]
+                                    .replace("$CHANNEL", channel)
+                                    .replace("$MASS", mass)
+                                    .replace("$PROCESS", sample)
+                                    .replace("$SYSTEMATIC", sys)
+                                ),
                             }
-                        )                      
+                        )
 
-    #log.debug("shapes dict: \n{}".format(json_str(shape_dict_list)))
-    
+    # log.debug("shapes dict: \n{}".format(json_str(shape_dict_list)))
+
     return shape_dict_list
 
 
@@ -100,11 +117,15 @@ def restructure_observations(observations: list, shapes_list: list) -> list:
         obs = float(observations[1].split()[1:][i_ch])  # could use int for data
         dct = {"name": channel}
         for shape_dict in shapes_list:
-            if shape_dict["sample"] == "data_obs" and shape_dict["channel"] == channel and "modifier" not in shape_dict:
-                log.debug("Found it!") 
-                dct["data"] = extract_histogram(shape_dict["file"], shape_dict["histogram"])
+            if (
+                shape_dict["sample"] == "data_obs"
+                and shape_dict["channel"] == channel
+                and "modifier" not in shape_dict
+            ):
+                log.debug("Found it!")
+                dct["data"] = f"{shape_dict['file']}:{shape_dict['histogram']}"
         obs_dict_list.append(dct)
-    #log.debug(f"\nobs dict (after restructuring observations):\n{json_str(obs_dict_list)}\n")
+    # log.debug(f"\nobs dict (after restructuring observations):\n{json_str(obs_dict_list)}\n")
     return obs_dict_list
 
 
@@ -122,25 +143,30 @@ def restructure_channels(samples: list) -> list:
         # get indices of current channel
         ch_idx = [i for i, c in enumerate(channel_names) if c == ch]
         sample_dict_list = []
-        for i_sam, (sample, sample_num) in enumerate(zip(
-            [sample_names[i] for i in ch_idx], 
-            [samples_numbers[i] for i in ch_idx])
-            ):
+        for i_sam, (sample, sample_num) in enumerate(
+            zip([sample_names[i] for i in ch_idx], [samples_numbers[i] for i in ch_idx])
+        ):
             # include a placeholder for sample modifiers
             sample_dict_list.append(
                 {
                     "name": sample,
-                    "id": sample_num, 
-                    "data": [yields[ch_idx[i_sam]]], 
-                    "modifiers": []}
+                    "id": sample_num,
+                    "data": [yields[ch_idx[i_sam]]],
+                    "modifiers": [],
+                }
             )
         ch_dict_list.append({"name": ch, "samples": sample_dict_list})
-    #log.debug(f"\nch dict:\n{json_str(ch_dict_list)}\n")
+    # log.debug(f"\nch dict:\n{json_str(ch_dict_list)}\n")
     return ch_dict_list
 
 
 def restructure_modifiers(
-    modifiers: list, channel_names: list, channel_yields: list, sample_names: list, sample_numbers: list, shape_identifiers: list = []
+    modifiers: list,
+    channel_names: list,
+    channel_yields: list,
+    sample_names: list,
+    sample_numbers: list,
+    shape_identifiers: list = [],
 ) -> dict:
     """build a dictionary with modifiers per sample from datacard"""
     n_processes = len(sample_names)
@@ -170,7 +196,7 @@ def restructure_modifiers(
         else:
             norm_effects = line_split[2 : 2 + n_processes]
         norm_effects = [float(n) if n != "-" else 0.0 for n in norm_effects]
-        #log.debug(f"syst {syst_name} with type {syst_type} and effects {norm_effects}")
+        # log.debug(f"syst {syst_name} with type {syst_type} and effects {norm_effects}")
 
         for i, norm_effect in enumerate(norm_effects):
             # go through each sample affected by a modifier
@@ -179,9 +205,9 @@ def restructure_modifiers(
             channel_name = channel_names[i]
             sample_name = sample_names[i]
             sample_number = sample_numbers[i]
-            #log.debug(
+            # log.debug(
             #    f" - norm effect {norm_effect} for {sample_name} in {channel_name}"
-            #)
+            # )
             if syst_type == "lnN":
                 if isinstance(norm_effect, float):
                     modifier_dict[channel_name][sample_name].append(
@@ -203,39 +229,47 @@ def restructure_modifiers(
             elif syst_type == "lnU":
                 # don't know exactly how this should be treated;
                 # have it like this for now, then we'll see
-                    modifier_dict[channel_name][sample_name].append(
-                        {
-                            "name": syst_name,
-                            "type": "normsys - lnU",
-                            "data": {"hi": norm_effect, "lo": 1 / norm_effect},
-                        }
-                    )               
+                modifier_dict[channel_name][sample_name].append(
+                    {
+                        "name": syst_name,
+                        "type": "normsys - lnU",
+                        "data": {"hi": norm_effect, "lo": 1 / norm_effect},
+                    }
+                )
             elif syst_type == "gmN":
                 # this needs access to channel yields to calculate absolute stat. unc.
                 abs_stat_unc = norm_effect * channel_yields[i]
                 modifier_dict[channel_name][sample_name].append(
                     {"name": syst_name, "type": "staterror", "data": [abs_stat_unc]}
                 )
-            elif syst_type == "shape" or syst_type == "shape?": # still have to understand what exactly shape? means
+            elif (
+                syst_type == "shape" or syst_type == "shape?"
+            ):  # still have to understand what exactly shape? means
                 # see chat with ACM for meaning of shape?
                 # first we scan shape_identifiers to find the modifier for this channel/sample combination
                 for shape_identifier in shape_identifiers:
-                    if "modifier" in shape_identifier and shape_identifier["sample"] == sample_name \
-                        and shape_identifier["channel"] == channel_name and shape_identifier["modifier"] == syst_name:
-                        histo_up = extract_histogram(shape_identifier["file"], shape_identifier["histogram_up"])
-                        histo_down = extract_histogram(shape_identifier["file"], shape_identifier["histogram_down"])
+                    if (
+                        "modifier" in shape_identifier
+                        and shape_identifier["sample"] == sample_name
+                        and shape_identifier["channel"] == channel_name
+                        and shape_identifier["modifier"] == syst_name
+                    ):
+                        histo_up = f"{shape_identifier['file']}:{shape_identifier['histogram_up']}"
+                        histo_down = f"{shape_identifier['file']}:{shape_identifier['histogram_down']}"
                         modifier_dict[channel_name][sample_name].append(
                             {
                                 "name": syst_name,
                                 "type": "shaperror",
                                 "data": norm_effect,
-                                "shape": {"up": histo_up, "down": histo_down}
+                                "shape": {"up": histo_up, "down": histo_down},
                             }
                         )
                         break
             else:
-                raise NotImplementedError("syst_type {} not supported".format(syst_type))
-    #log.debug(f"\nmodifier dict:\n{json_str(modifier_dict)}\n")
+                raise NotImplementedError(
+                    "syst_type {} not supported".format(syst_type)
+                )
+    # log.debug(f"\nmodifier dict:\n{json_str(modifier_dict)}\n")
     return modifier_dict
 
 
@@ -269,7 +303,9 @@ def get_sections_dict(datacard: list) -> dict:
     # root files for shapes analyses
     try:
         idx = next(
-            i for i, s in enumerate(sections_list) if any([l.startswith("shapes") for l in s])
+            i
+            for i, s in enumerate(sections_list)
+            if any([l.startswith("shapes") for l in s])
         )
         sections_dict.update({"shapes": sections_list.pop(idx)})
     except StopIteration:
@@ -287,7 +323,7 @@ def get_sections_dict(datacard: list) -> dict:
         i for i, s in enumerate(sections_list) if any(["rate" in l[0:4] for l in s])
     )
     sections_dict.update({"channels": sections_list.pop(idx)})
-    
+
     # UPDATED: systematics, last in list (need better identifier)
     # in the shapes configuration, last lines can be of format:
     # <something> autoMCStats <many something>
@@ -296,7 +332,10 @@ def get_sections_dict(datacard: list) -> dict:
     modifiers = []
     unsupported_modifiers = ["autoMCStats", "rateParam", "param"]
     for i, line in enumerate(sections_list[-1]):
-        if any(unsupported_modifier in line for unsupported_modifier in unsupported_modifiers):
+        if any(
+            unsupported_modifier in line
+            for unsupported_modifier in unsupported_modifiers
+        ):
             break
         else:
             modifiers.append(sections_list[-1].pop(i))
@@ -308,12 +347,14 @@ def get_sections_dict(datacard: list) -> dict:
     sample_names = sections_dict["channels"][1].split()[1:]
     sample_numbers = sections_dict["channels"][2].split()[1:]
     modifier_names = [line.split()[0] for line in sections_dict["modifiers"]]
-    #log.debug("Modifiers: {}".format(modifier_names))
+    # log.debug("Modifiers: {}".format(modifier_names))
 
     # restructure shapes
-    mass = "800"
+    mass = "200"
     if "shapes" in sections_dict:
-        sections_dict["shapes"] = restructure_shapes(sections_dict["shapes"], channel_names, sample_names, modifier_names, mass)
+        sections_dict["shapes"] = restructure_shapes(
+            sections_dict["shapes"], channel_names, sample_names, modifier_names, mass
+        )
     else:
         sections_dict["shapes"] = []
 
@@ -328,19 +369,17 @@ def get_sections_dict(datacard: list) -> dict:
     # convert modifier information into dict
     # needs access to full lists of channels (+ yields) and sample names
     sections_dict["modifiers"] = restructure_modifiers(
-        sections_dict["modifiers"], channel_names, channel_yields, sample_names, sample_numbers, sections_dict["shapes"]
+        sections_dict["modifiers"],
+        channel_names,
+        channel_yields,
+        sample_names,
+        sample_numbers,
+        sections_dict["shapes"],
     )
 
-    #log.debug("sections_dict = {}".format(sections_dict))
+    # log.debug("sections_dict = {}".format(sections_dict))
 
     return sections_dict
-
-
-def extract_histogram(file: str, histo: str) -> dict:
-    #log.debug("Extracting {} from {}".format(histo, file))
-    f = uproot.open(file)
-    histo = f[histo]
-    return list(histo.values())
 
 
 def sections_dict_to_workspace(sections_dict: dict) -> dict:
@@ -359,15 +398,24 @@ def sections_dict_to_workspace(sections_dict: dict) -> dict:
             if idn <= 0:
                 # this should be signal, attach normfactor
                 sample["modifiers"].append(
-                    {"data": None, "name": "r_{}".format(sample["name"]), "type": "normfactor"}
+                    {
+                        "data": None,
+                        "name": "r_{}".format(sample["name"]),
+                        "type": "normfactor",
+                    }
                 )
             # time to add shapes, if shapes analysis
             if sections_dict["shapes"]:
                 # loop to check which one matches all the requirements
                 for shape_dict in sections_dict["shapes"]:
-                    if "modifier" not in shape_dict and shape_dict["sample"] == sample["name"] \
-                        and shape_dict["channel"] == channel["name"]:
-                        sample["data"] = extract_histogram(shape_dict["file"], shape_dict["histogram"])
+                    if (
+                        "modifier" not in shape_dict
+                        and shape_dict["sample"] == sample["name"]
+                        and shape_dict["channel"] == channel["name"]
+                    ):
+                        sample[
+                            "data"
+                        ] = f"{shape_dict['file']}:{shape_dict['histogram']}"
                         break
 
     ws.update({"channels": sections_dict["channels"]})
@@ -383,13 +431,13 @@ def sections_dict_to_workspace(sections_dict: dict) -> dict:
 def datacard_to_json(datacard: list) -> dict:
     sections_dict = get_sections_dict(datacard)
     ws = sections_dict_to_workspace(sections_dict)
-    #log.debug(f"HistFactory workspace in JSON:\n{json_str(ws)}\n")
+    # log.debug(f"HistFactory workspace in JSON:\n{json_str(ws)}\n")
     return ws
 
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.DEBUG, format="%(levelname)s - %(message)s")
-    
+
     with open(sys.argv[-1]) as f:
         datacard = f.readlines()
 
